d5838becf79091c2e8da87deb30c854b54b13a62
Nataly <ncgarzon88@gmail.com>
2018-05-03 16:45:26 -0400

    Secret code works

--- dist/js/secret-code.js
+++ dist/js/secret-code.js
@@ -1,8 +1,26 @@
-"use strict";
+'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var SecretCode = function SecretCode(code, callback) {
+//how you encapsulate a bunch of code. With Classes we start with a capital letter
+var SecretCode = function SecretCode(secretCode, callback) {
 	_classCallCheck(this, SecretCode);
+
+	var keysPressed = '';
+
+	window.addEventListener('keyup', function () {
+
+		keysPressed += event.key;
+		console.log(keysPressed);
+
+		while (keysPressed.length > 100) {
+			keysPressed = keysPressed.substr(1);
+		}
+
+		if (keysPressed.includes(secretCode)) {
+			callback();
+			keysPressed = '';
+		}
+	});
 };
 //# sourceMappingURL=secret-code.js.map
--- src/js/secret-code.js
+++ src/js/secret-code.js
@@ -1,7 +1,25 @@
-class SecretCode {
 
-	constructor(code, callback) {
+
+//how you encapsulate a bunch of code. With Classes we start with a capital letter
+class SecretCode {
+	constructor(secretCode, callback) {
 		
+		let keysPressed = '';
+
+		window.addEventListener('keyup', function(){
+
+			keysPressed += event.key;
+			console.log(keysPressed);
+
+			while(keysPressed.length > 100){
+				keysPressed = keysPressed.substr(1);				
+			}
+
+			if (keysPressed.includes(secretCode)){
+				callback();
+				keysPressed = ''
+			}
+		})
 	
 	}
 }
